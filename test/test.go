60865bf2aff00f264a267991148ff0e249d53cb7
Sog0 <varsuscom@gmail.com>
2025-07-21 14:23:52 +0300

    minor fixes

--- test/test.go
+++ test/test.go
@@ -9,28 +9,31 @@ import (
 )
 
 func TestCheckInfrastructure(t *testing.T) {
-	
-	t.Parallel()
 
+	t.Parallel()
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../infrastructure",
 	}
 
-
 	terraform.Init(t, terraformOptions)
 
-
+	
 	instanceIDs := terraform.OutputList(t, terraformOptions, "instance_ids")
 	assert.Greater(t, len(instanceIDs), 0, "EC2 instances must be created")
 
-
+	
 	vpcID := terraform.Output(t, terraformOptions, "vpc_id")
 	vpc := aws.GetVpcById(t, vpcID, "us-east-1")
 	assert.Equal(t, "10.0.0.0/16", aws.GetCidrBlockFromVpc(t, vpc), "VPC CIDR should be 10.0.0.0/16")
 
+	
+	dbInstanceIDs := terraform.OutputList(t, terraformOptions, "db_instance_ids")
+	assert.Greater(t, len(dbInstanceIDs), 0, "DB instances must be created")
 
-	dbInstanceID := terraform.Output(t, terraformOptions, "db_instance_id")
-	dbInfo := aws.GetRdsInstanceDetails(t, "us-east-1", dbInstanceID)
-	assert.False(t, dbInfo.PubliclyAccessible, "Database must NOT be publicly accessible")
+	
+	for _, dbInstanceID := range dbInstanceIDs {
+		dbInfo := aws.GetRdsInstanceDetails(t, "us-east-1", dbInstanceID)
+		assert.False(t, dbInfo.PubliclyAccessible, "Database must NOT be publicly accessible")
+	}
 }

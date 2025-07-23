0f62ed66ae279f180111df3208934c3534a19dad
MVN <varsuscom@gmail.com>
2025-07-23 23:42:39 +0300

    added local for multiple envs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,4 @@
 provider "aws" {
   region = "us-east-1"
-}
-
-terraform {
-  backend "s3" {
-    bucket         = "bucketformyneeds1"
-    key            = "webserver/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile = true
-  }
+  profile = "Sog0"
 }
